761b7c63bbd5fec4c09cc93380fbf3ed3e1f867b
Michal Nithesh <151201789+Nithesh14@users.noreply.github.com>
2024-01-09 10:55:05 -0800

    Time Conversion.java

--- Time Conversion.java
+++ Time Conversion.java
@@ -6,69 +6,70 @@ public class TConverter
 public TConverter()
 {
 
-String hr = &quot;Hours&quot;;
-String min = &quot;Minutes&quot;;
-String sec = &quot;Seconds&quot;;
+String hr = "Hours";
+String min = "Minutes";
+String sec = "Seconds";
 double time = 0, rate=0;
 // Interface
-System.out.println(&quot;Welcome to the Time Converter Program \n&quot;);
-System.out.println(&quot;Use the following codes to input your Time choices: \n 1 - Hours \n 2 -
-Minutes \n 3 - Seconds \n&quot;);
-System.out.println(&quot;Please choose the input time format:&quot;);
+System.out.println("Welcome to the Time Converter Program \n");
+System.out.println("Use the following codes to input your Time choices: \n 1 - Hours \n 2 -
+Minutes \n 3 - Seconds \n");
+System.out.println("Please choose the input time format:");
 Scanner in = new Scanner(System.in);
 int choice = in.nextInt();
 String inType = null;
 switch(choice) {
-case 1: inType = &quot;Hours &gt;&gt; &quot; ; break;
-case 2: inType = &quot;Minutes &gt;&gt; &quot;; break;
-case 3: inType = &quot;Seconds &gt;&gt; &quot;; break;
+case 1: inType = "Hours >>" ; break;
+case 2: inType = "Minutes >>" ; break;
+case 3: inType = "Seconds >>" ; break;
 default:
-System.out.println(&quot;Please restart the program &amp; enter a number from the list.&quot;);
+System.out.println("Please restart the program &amp; enter a number from the list.");
 return;
 }
 
-System.out.println(&quot;Please choose the output time format&quot;);
+System.out.println("Please choose the output time format");
 int output = in.nextInt();
-System.out.printf(&quot;Now enter the input in &quot; + inType);
+System.out.println("Now enter the input in " + inType);
 double input = in.nextDouble();
 if (choice == output)
-System.out.println(&quot;Same time format no need to convert&quot;);
-if (choice == 1 &amp;&amp; output == 2)
+System.out.println("Same time format no need to convert");
+if (choice == 1 && output == 2)
 {
 double hrs_min_rate = 60;
 rate = input * hrs_min_rate;
-System.out.printf(input+ &quot; Hours converted to &quot;+rate+&quot; Minutes&quot;);
+System.out.println(input+ " Hours converted to "+rate+" Minutes");
 }
-else if (choice == 1 &amp;&amp; output == 3){
+else if (choice == 1 && output == 3){
 double hrs_sec_rate = 3600;
 rate = input * hrs_sec_rate;
-System.out.printf(input+ &quot; Hours converted to &quot;+rate+&quot; seconds&quot;);
+System.out.println(input+ " Hours converted to "+rate+" seconds");
 }
-if (choice == 2 &amp;&amp; output == 1)
+if (choice == 2 && output == 1)
 {
 double min_hrs_rate = 0.01667;
 rate = input * min_hrs_rate;
-System.out.printf(input+ &quot; Minutes converted to &quot;+rate+&quot; Hours&quot;);
+System.out.println(input+ " Minutes converted to "+rate+" Hours";);
 }
-else if (choice == 2 &amp;&amp; output == 3)
+else if (choice == 2 && output == 3)
 {
 double min_sec_rate = 60;
 rate = input * min_sec_rate;
-System.out.printf(input+ &quot; Minutes converted to &quot;+rate+&quot; Seconds&quot;);
+System.out.println(input+ " Minutes converted to "+rate+" Seconds");
 }
-if (choice == 3 &amp;&amp; output == 1)
+if (choice == 3 && output == 1)
 {
 
 double sec_hrs_rate = 0.0002778;
 rate = input * sec_hrs_rate;
-System.out.printf(input+ &quot; Seconds converted to &quot;+rate+&quot; Hours&quot;);
+System.out.println(input+ " Seconds converted to "+rate+" Hours");
 }
-else if (choice == 3 &amp;&amp; output == 2)
+else if (choice == 3 && output == 2)
 {
 double sec_min_rate = 0.01667;
 rate = input * sec_min_rate;
-System.out.printf(input+ &quot; Seconds converted to &quot;+rate+&quot; Minutes&quot;); }
-System.out.println(&quot;Thank you for using the Time converter&quot;);
+System.out.println(input+ " Seconds converted to "+rate+" Minutes");
+}
+System.out.println("Thank you for using the Time converter");
 }
 }
 Using Package:
